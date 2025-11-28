fe83581a37d20c5ceb319161af131cc043ef7f61
heathcliff4736 <lookout97@naver.com>
2025-11-28 10:57:32 +0900

    feat:mainController setting

---
+++ src/main/java/com/ssg/noranekoshop/common/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.ssg.noranekoshop.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = {"/", "{path:[^.]*}", "{path1:[^.]*}/{path2:[^.]*}"})
+    public String index() {
+        return "/index.html";
+    }
+}
